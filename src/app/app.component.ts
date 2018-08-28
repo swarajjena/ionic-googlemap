367438dddcd1534ab03fa5163a7584e8ab8eff12
swaraj <jena.swaraj@gmail.com>
2018-08-28 10:21:58 +0530

    j

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,7 @@ import { DetailPage } from '../pages/detail/detail';
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = DetailPage;
+  rootPage:any = HomePage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
